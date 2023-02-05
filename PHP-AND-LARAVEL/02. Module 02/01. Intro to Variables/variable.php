a87d9620a17cbb7c1218b7406362def2e8187b4b
Somel Ahmed <somelahmed88@gmail.com>
2023-02-05 16:43:18 +0600

    introduction to Variable added

--- PHP-AND-LARAVEL/02. Module 02/01. Intro to Variables/variable.php
+++ PHP-AND-LARAVEL/02. Module 02/01. Intro to Variables/variable.php
@@ -3,3 +3,14 @@ $name = "Bangladesh";
 $age = 16;
 $word = 'age';
 echo $$word;
+
+echo "\n";
+
+//? echo "Hello", $name;
+echo "Hello $name";
+
+//? echo 'Hello $name';//It will not work because it will be act as an String
+
+echo "\n";
+echo "Hello {$name}\n";
+
