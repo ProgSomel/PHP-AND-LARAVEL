4446ea10fffe01353d860ff406eddab17bc3dffd
Somel Ahmed <somelahmed88@gmail.com>
2023-02-05 18:08:47 +0600

    numbersSystem.php added

---
+++ PHP-AND-LARAVEL/02. Module 02/04. Discussion of different number systems/numberSystem.php
@@ -0,0 +1,20 @@
+<?php
+$n = 123;
+//! -------------Octal---------------
+$o = 012;
+//! ----------Hexadecimal----------------
+printf( "The number is %d and %d and 5d", $n, $o, $h );
+
+//! ---Converting to binary Number--------
+echo "\n";
+printf( "The Binary conversion of %d is %b \n", 1212, 1212 );
+
+//! ---Converting to binary Number--------
+echo "\n";
+printf( "The Binary conversion of %d is %x \n", 1212, 1212 );
+
+//! -------------Octal---------------
+echo "\n";
+printf( "The Binary conversion of %d is %o \n", 27, 27 );
+
+printf( "Octal %o = %d", 0200000, 0200000 );
